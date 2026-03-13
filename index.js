3e48416810707eaa02aa8ce15aa166783e270bef
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-03-13 23:00:05 +0600

    4th commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function changetext(){
           document.getElementById("text").innerText="text changed";
 }
+
+function increment(){
+  let number = document.getElementById("count").innerText;
+  number++;
+  document.getElementById("count").innerText = number;
+}
